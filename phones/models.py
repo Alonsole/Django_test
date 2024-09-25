f695eb5bdaa362f9e0c668911f4c22fe337350f5
Alonsole <157732573+Alonsole@users.noreply.github.com>
2024-09-25 23:16:07 +0300

    Update models.py

--- phones/models.py
+++ phones/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    id = models.IntegerField(primary_key=True, null=False)
     name = models.CharField(max_length=255, null=False)
     price = models.BigIntegerField(null=True)
-    image = models.CharField()
+    image = models.URLField(null=True)
     release_date = models.DateField(null=True)
     lte_exists = models.BooleanField()
-    slug = models.SlugField(editable=False)
+    slug = models.SlugField(unique=True, editable=False)
 
